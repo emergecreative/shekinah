04ab8b1771acb40d0ffa15f383ea320175518468
Emerge Creative <contato@emergecreative.com.br>
2021-12-01 11:35:39 -0300

    Ajustes Diversos

--- email/EmailTemplate/teste.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $nome = $_POST["nome"];
-    $telefone = $_POST["telefone"];
-    $email = $_POST["email"];
-    $mensagem = $_POST["mensagem"];
-    $tipo = $_POST["tipo"];
-
-    echo "$nome <br> $telefone <br> $email <br> $mensagem <br> $tipo";
-?>
